e87eb00c478019b223bf335052aaa8b633e6a6d0
Johnny Ji <johnny.ji@live.ca>
2015-02-23 23:14:17 -0800

    retouching CSS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
 
+	$(".title").addClass("animated bounceInDown");
+	
+	setTimeout(function() {
+		$(".title").removeClass("animated bounceInDown");
+	}, 1000);
+
 	//bounces the contact me text
 	$(".animation").hover(function() {
 		$(this).addClass("animated bounce");
 		setTimeout(function() {
 			$(".animation").removeClass("animated bounce");
-		}, 1000);
+		}, 1200);
 	});
 
 });
